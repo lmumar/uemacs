691cbdce6321519f66b071326168b2957390acf3
Lord Norlan Mumar <kuling.guwapo@gmail.com>
2016-05-16 15:31:18 +0800

    Fix overlapping memory problem in OSX

--- file.c
+++ file.c
@@ -246,7 +246,8 @@ int readin(char *fname, int lockfl)
 	if ((s = bclear(bp)) != TRUE)	/* Might be old.        */
 		return s;
 	bp->b_flag &= ~(BFINVS | BFCHG);
-	strcpy(bp->b_fname, fname);
+	if (bp->b_fname != fname)
+		strcpy(bp->b_fname, fname);
 
 	/* let a user macro get hold of things...if he wants */
 	execute(META | SPEC | 'R', FALSE, 1);
